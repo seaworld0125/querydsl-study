0572963f69afdf992196e3fd170cff34997e6ac2
Tae kyoung Kim <mabrother98@gmail.com>
2022-12-02 13:34:22 +0900

    [💡 TIL] db, 로깅 세팅 및 검증

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
@@ -13,6 +14,7 @@ import javax.persistence.EntityManager;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
 	@Autowired
@@ -23,6 +25,8 @@ class QuerydslApplicationTests {
 
 		Hello hello = new Hello();
 		em.persist(hello);
+		em.flush();
+		em.clear();
 
 		JPAQueryFactory query = new JPAQueryFactory(em);
 		QHello qHello = QHello.hello;
@@ -31,7 +35,7 @@ class QuerydslApplicationTests {
 				.selectFrom(qHello)
 				.fetchOne();
 
-		Assertions.assertThat(res).isEqualTo(hello);
+		Assertions.assertThat(res.getId()).isEqualTo(hello.getId());
 	}
 
 }
